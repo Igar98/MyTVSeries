cf7ad050dce628247d213c7aad9b0d3c62d6bc68
Anselmo <igarsan98@gmail.com>
2024-10-30 10:47:09 +0100

    Commit with the initial project structure.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MyTVSeries
 An application to manage the ratings of the television series I'm watching.
+
+
+.Env values:
+
+POSTGRES_DB=
+POSTGRES_USER=
+POSTGRES_PASSWORD=
+
+H2_USERNAME=
+H2_PASSWORD=
---
+++ src/main/java/com/example/monolith/MyTvSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTvSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTvSeriesApplication.class, args);
+	}
+
+}
